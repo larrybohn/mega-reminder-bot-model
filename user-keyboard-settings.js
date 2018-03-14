b120ad46b12dacea7873fd4b214c39d3e858744c
Larry Bohn <larrybohn@users.noreply.github.com>
2018-03-15 02:47:14 +0300

    Commented out development keyboard settings.

--- user-keyboard-settings.js
+++ user-keyboard-settings.js
@@ -6,7 +6,7 @@ export default class UserKeyboardSettings {
 
     static GetDefault(userId) {
         return new UserKeyboardSettings(userId, [
-            [1, 2, 5, 10], //todo: remove, it's development only
+            //[1, 2, 5, 10], //todo: remove, it's development only
             [1*60, 5*60, 15*60, 30*60],
             [1*3600, 2*3600, 4*3600, 8*3600],
             [1*24*3600, 2*24*3600, 7*24*3600]            
