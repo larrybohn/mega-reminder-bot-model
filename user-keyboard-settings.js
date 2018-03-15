bafffe580787343b47d04e065a87c65cc6add923
Larry Bohn <larrybohn@users.noreply.github.com>
2018-03-15 14:24:24 +0300

    Changed default keyboard settings to include debug buttons depending on flag.

--- user-keyboard-settings.js
+++ user-keyboard-settings.js
@@ -4,12 +4,15 @@ export default class UserKeyboardSettings {
         this.buttons = buttons;
     }
 
-    static GetDefault(userId) {
-        return new UserKeyboardSettings(userId, [
-            //[1, 2, 5, 10], //todo: remove, it's development only
+    static GetDefault(userId, includeDebugButtons) {
+        let keyboardItems = [            
             [1*60, 5*60, 15*60, 30*60],
             [1*3600, 2*3600, 4*3600, 8*3600],
             [1*24*3600, 2*24*3600, 7*24*3600]            
-        ])
+        ];
+        if (includeDebugButtons) {
+            keyboardItems.unshift([1, 2, 5, 10]);
+        }
+        return new UserKeyboardSettings(userId, keyboardItems);
     }
 }
